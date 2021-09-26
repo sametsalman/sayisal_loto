2c99b91ceb66ee5225cf7d541541a4839cbd675a
sametsalman <sametsalmankk@gmail.com>
2021-09-26 22:27:32 +0300

    first repo
    ilk

---
+++ sayısalloto_ödev/sayısalloto_ödev/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace sayısalloto_ödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(82, 22);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(247, 32);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(82, 22);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(353, 32);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(82, 22);
+            this.textBox4.TabIndex = 0;
+            this.textBox4.Text = "0";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(458, 32);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(82, 22);
+            this.textBox5.TabIndex = 0;
+            this.textBox5.Text = "0";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(564, 32);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(82, 22);
+            this.textBox6.TabIndex = 0;
+            this.textBox6.Text = "0";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(52, 173);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(82, 22);
+            this.textBox7.TabIndex = 0;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(158, 173);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(82, 22);
+            this.textBox8.TabIndex = 0;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(264, 173);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(82, 22);
+            this.textBox9.TabIndex = 0;
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(370, 173);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(82, 22);
+            this.textBox10.TabIndex = 0;
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(475, 173);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(82, 22);
+            this.textBox11.TabIndex = 0;
+            this.textBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(581, 173);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(82, 22);
+            this.textBox12.TabIndex = 0;
+            this.textBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(17, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(670, 80);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(561, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 61);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(52, 242);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(55, 53);
+            this.panel2.TabIndex = 3;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(158, 242);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(55, 53);
+            this.panel3.TabIndex = 3;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(273, 242);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(55, 53);
+            this.panel4.TabIndex = 3;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(379, 242);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(55, 53);
+            this.panel5.TabIndex = 3;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(475, 242);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(55, 53);
+            this.panel6.TabIndex = 3;
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(581, 242);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(55, 53);
+            this.panel7.TabIndex = 3;
+            this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 549);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+    }
+}
+
---
+++ sayısalloto_ödev/sayısalloto_ödev/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayısalloto_ödev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static int c_say = 0;
+        public static int[] c_sayilar = new int[6];
+        public static TextBox[] cekilis_pan = new TextBox[6]; 
+
+
+        public static int Random_say_cen()
+        {
+            Random rmd1 = new Random();
+
+            c_say = rmd1.Next(100000,999999);
+
+
+            return c_say ;
+
+        }
+
+      
+        
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            bool form_tm = false;
+            bool t7 = false;
+            bool t8 = false;
+            bool t9 = false;
+            bool t10 = false;
+            bool t11 = false;
+            bool t12 = false;
+
+            /*
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+            textBox10.Clear();
+            textBox11.Clear();
+            textBox12.Clear();
+            */
+
+
+
+
+            // int test_orn = 123456;
+            if (Convert.ToInt32( textBox7.Text) >= 0 || Convert.ToInt32(textBox7.Text) <= 9) 
+            {
+                t7 = true;
+            };
+            if (Convert.ToInt32(textBox8.Text) >= 0 && Convert.ToInt32(textBox8.Text) <= 9)
+            {
+                t8 = true;
+            }
+            if (Convert.ToInt32(textBox9.Text) >= 0 && Convert.ToInt32(textBox9.Text) <= 9)
+            {
+                t9 = true;
+            }
+            if (Convert.ToInt32(textBox10.Text) >= 0 && Convert.ToInt32(textBox10.Text) <= 9)
+            {
+                t10 = true;
+            }
+            if (Convert.ToInt32(textBox11.Text) >= 0 && Convert.ToInt32(textBox11.Text) <= 9)
+            {
+                t11 = true;
+            }
+            if (Convert.ToInt32(textBox12.Text) >= 0 && Convert.ToInt32(textBox12.Text) <= 9)
+            {
+                t12 = true;
+            }
+
+            if (t7 && t9 && t10 && t11 && t12 == true )
+            {
+                form_tm = true;
+            }
+
+            if (form_tm == true) {
+
+                Random_say_cen();
+
+
+                int sec_say = c_say;
+
+                char[] sayı_c;
+
+
+
+                string sayı = sec_say.ToString();
+
+
+                sayı_c = sayı.ToCharArray();
+
+
+                /*int i = 0;
+                textBox1.Text = c_say.ToString();
+                foreach (char c in sayı_c) 
+
+                {
+
+                     //syı[i] = c;
+
+                };
+
+                //  textBox6.Text = syı[5].ToString();
+
+                //c_say.ToString*/
+
+                cekilis_pan[1] = textBox2;
+                cekilis_pan[2] = textBox3;
+                cekilis_pan[3] = textBox4;
+                cekilis_pan[4] = textBox5;
+                cekilis_pan[5] = textBox6;
+
+
+                char[] syı = sayı.ToCharArray(0, 6);
+                //textBox1.Tex
+                textBox5.Text = syı[4].ToString();
+                textBox1.Text = syı[0].ToString();
+                textBox2.Text = syı[1].ToString();
+                textBox3.Text = syı[2].ToString();
+                textBox4.Text = syı[3].ToString();
+                textBox6.Text = syı[5].ToString();
+
+
+
+                if (textBox1.Text == textBox7.Text ) 
+                {
+                    panel2.BackColor = Color.Green;
+
+
+                }
+                else 
+                {
+                    panel2.BackColor = Color.Red;
+                }
+
+
+                if (textBox2.Text == textBox8.Text)
+                {
+                    panel3.BackColor = Color.Green;
+
+
+                }
+                else
+                {
+                    panel3.BackColor = Color.Red;
+                }
+
+
+                if (textBox3.Text == textBox9.Text)
+                {
+                    panel4.BackColor = Color.Green;
+
+
+                }
+                else
+                {
+                    panel4.BackColor = Color.Red;
+                }
+
+                if (textBox4.Text == textBox10.Text)
+                {
+                    panel5.BackColor = Color.Green;
+
+
+                }
+                else
+                {
+                    panel5.BackColor = Color.Red;
+                }
+
+
+                if (textBox5.Text == textBox11.Text)
+                {
+                    panel6.BackColor = Color.Green;
+
+
+                }
+                else
+                {
+                    panel6.BackColor = Color.Red;
+                }
+
+
+                if (textBox6.Text == textBox12.Text)
+                {
+                    panel7.BackColor = Color.Green;
+
+
+                }
+                else
+                {
+                    panel7.BackColor = Color.Red;
+                }
+
+
+                /*cekilis_pan[0].Text = syı[0].ToString();
+                cekilis_pan[1].Text = syı[1].ToString();
+                cekilis_pan[2].Text = syı[2].ToString();
+                cekilis_pan[3].Text = syı[3].ToString();
+                cekilis_pan[4].Text = syı[4].ToString();
+                cekilis_pan[5].Text = syı[5].ToString();*/
+
+
+
+
+
+
+                /*int a =0;
+
+
+                int sayı_U1 = 0;
+                int sayı_U2 = 5;
+                int sayı_U3 = 0;
+                int sayı_U4 = 0;
+                int sayı_U5 = 0;
+                int sayı_U6 = 0;
+
+
+                //sayı_U1 = c_say;
+
+                cekilis_pan[1] = textBox2;
+                cekilis_pan[2] = textBox3;
+                cekilis_pan[3] = textBox4;
+                cekilis_pan[4] = textBox5;
+                cekilis_pan[5] = textBox6;
+
+
+
+                /*
+                cekilis_pan[0].Text = sayı_U1.ToString();
+                cekilis_pan[1].Text = sayı_U2.ToString();
+                cekilis_pan[2].Text = sayı_U3.ToString();
+                cekilis_pan[3].Text = sayı_U4.ToString();
+                cekilis_pan[4].Text = sayı_U5.ToString();
+                cekilis_pan[5].Text = sayı_U6.ToString();*/
+
+
+                // int rep_sec = 0;
+                // rep_sec = cekilis_pan.Text.
+
+                /* int i = 0;
+                 while(i <= 5) 
+                 {
+                     Random_say_cen();
+
+                     cekilis_pan[i].Text = c_say.ToString();
+
+
+                         /*if ( == true) 
+                         {
+
+                         }
+
+
+
+                     i++;
+
+                 };*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
